fdf4d5a23d02598fa8a446550d5326040cc1e979
DaffaPutra7 <putradika3604@gmail.com>
2023-07-02 16:05:41 +0700

    Membuat void postorder

--- binarysearchtree_133/binarysearchtree_133.cpp
+++ binarysearchtree_133/binarysearchtree_133.cpp
@@ -89,5 +89,16 @@ public:
 		}
 	}
 
-
+	void postorder(Node* ptr) { // Preforms the postorder traversal of the tree
+		if (ROOT == NULL) {
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL) {
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << "  ";
+		}
+	}
 };
+
